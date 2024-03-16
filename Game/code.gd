d4da8be007a3b8b761468e959f7d926e20c071a1
mm11762 <MatthewMa929@Gmail.com>
2024-03-16 11:14:20 -0400

    x_bar
    considering duplicating instead of creating copies

--- Game/code.gd
+++ Game/code.gd
@@ -2,17 +2,18 @@ extends Node2D
 
 #Code start, timer start, timer resets when space is pressed, if timer timeout then one empty space
 
-@onready var code_bar = $CodeBarProgress
 @onready var code_bar_timer = $CodeBarTimer
+@onready var code_bar = $CodeBar
 @onready var lit_bar = $LitBar
 @onready var lit_bar2 = $LitBar2
 @onready var lit_bar3 = $LitBar3
 @onready var lit_bar4 = $LitBar4
 @onready var lit_bar5 = $LitBar5
+@onready var x_bar = $XBar
+@onready var iterator = 0
+@onready var lit_list = [lit_bar, lit_bar2, lit_bar3, lit_bar4, lit_bar5]
 
 var on = true
-var done = false
-var num = 300.0/593
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,25 +22,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if on:
-		done = false
-		code_bar.value += 1
-	if code_bar.value == code_bar.max_value:
-		on = false
-		done = true
-	if Input.is_action_pressed("Light") and on:
-		print(num)
-		if code_bar.value > 0*num and code_bar.value < 100*num:
-			lit_bar.visible = true
-		if code_bar.value > 100*num and code_bar.value < 200*num:
-			lit_bar2.visible = true
-		if code_bar.value > 200*num and code_bar.value < 300*num:
-			lit_bar3.visible = true
-		if code_bar.value > 300*num and code_bar.value < 400*num:
-			lit_bar4.visible = true
-		if code_bar.value > 400*num and code_bar.value < 500*num:
-			lit_bar5.visible = true
-
-
+		code_bar.visible = true
+		
+	if Input.is_action_just_pressed("Light") and on and iterator < 5:
+		lit_list[iterator].visible = true
+		iterator += 1
+		code_bar_timer.start()
+		
 func _on_code_bar_timer_timeout():
-	pass
-
+	iterator += 1
